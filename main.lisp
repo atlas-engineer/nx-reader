f7f32bee5250e021ef24013e74febba02fe50266
John Mercouris <john@mercouris.email>
2020-05-23 21:56:23 +0200

    main.lisp: add function to generate HTML for feeds

--- main.lisp
+++ main.lisp
@@ -4,9 +4,13 @@
   (:use :common-lisp :next)
   (:export
    #:rss-site
-   #:rss-generate-html))
+   #:rss-generate-html
+   #:rss-urls
+   #:rss-feeds-generate-html))
 
 (in-package :nx-reader)
+
+(defparameter rss-urls (list))
 
 (defun rss-site (url)
   "Retrieve the RSS from a specific feed and parse it into an RSS object."
@@ -15,12 +19,28 @@
 (defun rss-generate-html (rss-object)
   "Generate the HTML representing a particular RSS feed."
   (markup:markup
-   (:h1 (rss:title rss-object))
-   (:p (:a :href (rss:link rss-object) (rss:link rss-object)))
-   (:p (rss:description rss-object))
-   (:ul (loop for item in (rss:items rss-object)
-              collect
-              (markup:markup (:li (:p (:b (rss:title item)))
-                                  (:p (:a :href (rss:link item) (rss:link item)))
-                                  (:p (markup:raw (rss:description item)))))))))
+   (:details 
+    :open "open"
+    (:summary (:h1 :style "display:inline" (rss:title rss-object)))
+    (:p (:a :href (rss:link rss-object) (rss:link rss-object)))
+    (:p (rss:description rss-object))
+    (:ul (loop for item in (rss:items rss-object)
+               collect
+               (markup:markup (:li (:p (:b (rss:title item)))
+                                   (:p (:a :href (rss:link item) (rss:link item)))
+                                   (:p (markup:raw (rss:description item))))))))))
 
+(defun rss-feeds-generate-html (&optional (rss-urls rss-urls))
+  (markup:markup
+   (:span (loop for rss-url in rss-urls collect
+                (rss-generate-html (rss-site rss-url))))))
+
+(define-command show-rss-feeds ()
+  "Show RSS Feeds"
+  (let* ((rss-buffer (make-buffer :title "*RSS Feed*"))
+         (rss-buffer-contents (rss-generate-html (rss-site "https://news.ycombinator.com/rss")))
+         (insert-contents (ps:ps (setf (ps:@ document Body |innerHTML|)
+                                       (ps:lisp rss-buffer-contents)))))
+    (ffi-buffer-evaluate-javascript rss-buffer insert-contents)
+    (set-current-buffer rss-buffer)
+    rss-buffer))
